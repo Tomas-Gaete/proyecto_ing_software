2dcb9fc45611e78ba0e5956fa5021400e4c3775d
Matías Olea <matolea@alumnos.uai.cl>
2024-10-12 18:40:54 -0300

    style improvements

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
       name: "Mathematics",
       startTime: "09:00",
       endTime: "10:30",
+      building: "A",
       location: "Room 101",
     },
     {
@@ -18,6 +19,7 @@ export default function Home() {
       name: "Computer Science",
       startTime: "11:00",
       endTime: "12:30",
+      building: "A",
       location: "Lab 3",
     },
     {
@@ -25,6 +27,7 @@ export default function Home() {
       name: "Physics",
       startTime: "14:00",
       endTime: "15:30",
+      building: "B",
       location: "Room 205",
     },
     {
@@ -32,24 +35,26 @@ export default function Home() {
       name: "English Literature",
       startTime: "16:00",
       endTime: "17:30",
+      building: "C",
       location: "Room 302",
     },
     {
-      id: 4,
+      id: 7,
+      name: "English Literature",
+      startTime: "16:00",
+      endTime: "17:30",
+      building: "A",
+      location: "Room 302",
+    },
+    {
+      id: 5,
       name: "English Literature",
       startTime: "16:00",
       endTime: "17:30",
       location: "Room 302",
     },
     {
-      id: 4,
-      name: "English Literature",
-      startTime: "16:00",
-      endTime: "17:30",
-      location: "Room 302",
-    },
-    {
-      id: 4,
+      id: 6,
       name: "English Literature",
       startTime: "16:00",
       endTime: "17:30",
@@ -69,18 +74,18 @@ export default function Home() {
     <div className="h-screen">
       <Nav />
       <div className="container mx-auto">
-        
         <main className="flex-grow container mx-auto px-4 py-8">
-          
           <div className="border rounded-lg shadow-md p-6 bg-white dark:bg-black">
-          <h1 className="text-3xl font-semibold text-center my-2">
-          Horario Peñalolen
-        </h1>
+            <h1 className="text-3xl font-semibold text-center my-2">
+              Horario Peñalolen
+            </h1>
             <div className="border-b pb-4 mb-4">
               <h3 className="text-black text-lg font-bold text-center mx-0 w-full dark:text-gray-200">
                 {formatDate(currentDate)}
               </h3>
-              <h6 className="text-center text-sm mx-0 w-full text-zinc-500">Última actualizacíon hace: nasfdasd</h6>
+              <h6 className="text-center text-sm mx-0 w-full text-zinc-500">
+                Última actualizacíon hace: Platanito
+              </h6>
             </div>
             <div>
               {schedule.length > 0 ? (
@@ -100,7 +105,35 @@ export default function Home() {
                         </p>
                       </div>
                       <div>
-                        <p className="text-sm text-zinc-400">{item.location}</p>
+                        <p className="">
+                          {(() => {
+                            let style;
+                            switch (item.building) {
+                              case "A":
+                                style =
+                                  "bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400";
+                                  console.log(item);
+                                break;
+                              case "B":
+                                style =
+                                  "bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-400 border border-yellow-500";
+                                break;
+
+                              case "C":
+                                style =
+                                  "bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-500";
+                                break;
+
+                              default:
+                                style =
+                                  "bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500";
+                                break;
+                            }
+                            return (
+                              <span className={style}>{item.location}</span>
+                            );
+                          })()}
+                        </p>
                       </div>
                     </li>
                   ))}
