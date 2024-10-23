a15c26b1914e23bcdab57b8f9fb7fff44f74a2b7
Felipe700 <127786670+Felipe700@users.noreply.github.com>
2024-10-23 15:43:23 -0300

    fix error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -67,8 +67,8 @@ const LoginPage: React.FC = () => {
                   </a>
             </div>
 
-            <div className="mt-4">
-              <a href="/">
+            <div className="mt-4 w-12">
+              <a href="/" className="w-12">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
               </svg> 
