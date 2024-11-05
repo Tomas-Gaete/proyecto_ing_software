2ac9d6ea5adb56793ff04ffe83b8857167f93cde
Matías Olea <matolea@alumnos.uai.cl>
2024-11-04 23:08:22 -0300

    added button to update schedule

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,137 +2,94 @@
 import { useState } from "react";
 
 export default function Nav() {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <nav className="block w-full max-w-screen-lg px-4 py-2 mx-auto bg-white dark:bg-black  shadow-md rounded-md lg:px-8 lg:py-3 ">
-      <div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800 dark:text-gray-200">
-        <a
-          href="#"
-          className="mr-4 block cursor-pointer py-1.5 text-base  dark:text-gray-200 text-slate-800 font-semibold"
-        >
-          Horario UAIN'T
-        </a>
-        <div className="hidden lg:block">
-          <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <li className="flex items-center p-1 text-sm gap-x-2  dark:text-gray-200 text-slate-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-6 w-6 text-slate-500"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-              <a href="login" className="flex items-center">
-                Iniciar Sesión
-              </a>
-            </li>
-            <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-6 w-6 text-slate-500"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-                />
-              </svg>
-              <a href="#" className="flex items-center">
-                Cursos
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="items-center hidden gap-x-2 lg:flex">
-          <div className="w-full max-w-sm min-w-[200px]">
-            <div className="relative">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                />
-              </svg>
-              <input
-                type="text"
-                className="w-full pl-10 pr-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-600 text-sm border border-slate-200 rounded-md transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                placeholder="Type here..."
-              />
-            </div>
-          </div>
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-          >
-            Search
-          </button>
-        </div>
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
-          type="button"
-        >
-          <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </span>
-        </button>
-      </div>
-      <div className={`${isOpen ? "block" : "hidden"} w-full`}>
-        <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-          {/* Add each list item here */}
-          <li className="flex items-center p-1 text-sm gap-x-2 dark:text-gray-200 text-slate-600">
-            <a href="#" className="flex items-center">
-              Pages
-            </a>
-          </li>
-          <li className="flex items-center p-1 text-sm gap-x-2  dark:text-gray-200 text-slate-600">
-            <a href="/login" className="flex items-center">
-              Iniciar Sesión
-            </a>
-          </li>
-          <li className="flex items-center p-1 text-sm gap-x-2  dark:text-gray-200 text-slate-600">
-            <a href="#" className="flex items-center">
-              Blocks
-            </a>
-          </li>
-          <li className="flex items-center p-1 text-sm gap-x-2  dark:text-gray-200 text-slate-600">
-            <a href="#" className="flex items-center">
-              Docs
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+	const [isOpen, setIsOpen] = useState(false);
+
+	const updateUserSchedule = async () => {
+		// Update the user's schedule
+	}
+
+	const updateDaySchedule = async () => {
+		// Update the room's schedule
+	}
+
+
+	return (
+		<nav className="block w-full max-w-screen-lg px-4 py-2 mx-auto bg-white dark:bg-black dark:shadow-[#212121] shadow-lg rounded-md lg:px-8 lg:py-3 ">
+			<div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800 dark:text-gray-200">
+				<a
+					href="/"
+					className="mr-4 block cursor-pointer py-1.5 text-base  dark:text-gray-200 text-slate-800 font-semibold"
+				>
+					Horario UAIN'T
+				</a>
+				<div className="hidden lg:block">
+			
+					
+				</div>
+				<div className="items-center hidden gap-x-2 lg:flex ">
+					<div className="w-full flex flex-row max-w-sm min-w-[200px]">
+					<button
+							type="button"
+							onClick={updateUserSchedule}
+							className="text-white w-full bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-bold rounded-lg text-sm px-3 py-2.5 text-center me-2 "
+						>
+							Actualizar Horario
+						</button>
+						<button
+							type="button"
+							onClick={updateDaySchedule}
+							className="text-white w-full bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-bold rounded-lg text-sm px-3 py-2.5 text-center me-2"
+						>
+							Actualizar Salas Hoy
+						</button>
+						
+					</div>
+				</div>
+				<button
+					onClick={() => setIsOpen(!isOpen)}
+					className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
+					type="button"
+				>
+					<span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="w-6 h-6"
+							fill="none"
+							stroke="currentColor"
+							strokeWidth="2"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M4 6h16M4 12h16M4 18h16"
+							></path>
+						</svg>
+					</span>
+				</button>
+			</div>
+			<div className={`${isOpen ? "block" : "hidden"} w-full`}>
+				<ul className="flex flex-col border-t-2 dark:border-gray-200 border-black gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+					{/* Add each list item here */}
+					<li className="flex items-center p-1 text-sm gap-x-2 dark:text-gray-200 text-slate-600">
+						<button
+							type="button"
+							onClick={updateUserSchedule}
+							className="text-white w-full bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-2"
+						>
+							Actualizar Horario
+						</button>
+					</li>
+					<li className="flex items-center p-1 text-sm gap-x-2 dark:text-gray-200 text-slate-600">
+						<button
+							type="button"
+							onClick={updateDaySchedule}
+							className="text-white w-full bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
+						>
+							Actualizar Salas Hoy
+						</button>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,193 +3,193 @@ import Nav from "./components/Nav";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const [schedule, setSchedule] = useState([
-    {
-      id: 1,
-      name: "Mathematics",
-      startTime: "09:00",
-      endTime: "10:30",
-      building: "A",
-      location: "Room 101",
-    },
-    {
-      id: 2,
-      name: "Computer Science",
-      startTime: "11:00",
-      endTime: "12:30",
-      building: "A",
-      location: "Lab 3",
-    },
-    {
-      id: 3,
-      name: "Physics",
-      startTime: "14:00",
-      endTime: "15:30",
-      building: "B",
-      location: "Room 205",
-    },
-    {
-      id: 4,
-      name: "English Literature",
-      startTime: "16:00",
-      endTime: "17:30",
-      building: "C",
-      location: "Room 302",
-    },
-    {
-      id: 7,
-      name: "English Literature",
-      startTime: "16:00",
-      endTime: "17:30",
-      building: "A",
-      location: "Room 302",
-    },
-    {
-      id: 5,
-      name: "English Literature",
-      startTime: "16:00",
-      endTime: "17:30",
-      location: "Room 302",
-    },
-    {
-      id: 6,
-      name: "English Literature",
-      startTime: "16:00",
-      endTime: "17:30",
-      location: "Room 302",
-    },
-  ]);
+	const [isOpen, setIsOpen] = useState(false);
+	const [currentDate, setCurrentDate] = useState(new Date());
+	const [schedule, setSchedule] = useState([
+		{
+			id: 1,
+			name: "Mathematics",
+			startTime: "09:00",
+			endTime: "10:30",
+			building: "A",
+			location: "Room 101",
+		},
+		{
+			id: 2,
+			name: "Computer Science",
+			startTime: "11:00",
+			endTime: "12:30",
+			building: "A",
+			location: "Lab 3",
+		},
+		{
+			id: 3,
+			name: "Physics",
+			startTime: "14:00",
+			endTime: "15:30",
+			building: "B",
+			location: "Room 205",
+		},
+		{
+			id: 4,
+			name: "English Literature",
+			startTime: "16:00",
+			endTime: "17:30",
+			building: "C",
+			location: "Room 302",
+		},
+		{
+			id: 7,
+			name: "English Literature",
+			startTime: "16:00",
+			endTime: "17:30",
+			building: "A",
+			location: "Room 302",
+		},
+		{
+			id: 5,
+			name: "English Literature",
+			startTime: "16:00",
+			endTime: "17:30",
+			location: "Room 302",
+		},
+		{
+			id: 6,
+			name: "English Literature",
+			startTime: "16:00",
+			endTime: "17:30",
+			location: "Room 302",
+		},
+	]);
 
-  const formatDate = (date: Date) => {
-    return date.toLocaleDateString("es-US", {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-    });
-  };
+	const formatDate = (date: Date) => {
+		return date.toLocaleDateString("es-US", {
+			weekday: "long",
+			month: "long",
+			day: "numeric",
+		});
+	};
 
-  return (
-    <div className="h-screen">
-      <Nav />
-      <div className="container mx-auto">
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <div className="border rounded-lg shadow-md p-6 bg-white dark:bg-black">
-            <h1 className="text-3xl font-semibold text-center my-2">
-              Horario Peñalolen
-            </h1>
-            <div className="border-b pb-4 mb-4">
-              <h3 className="text-black text-lg font-bold text-center mx-0 w-full dark:text-gray-200">
-                {formatDate(currentDate)}
-              </h3>
-              <h6 className="text-center text-sm mx-0 w-full text-zinc-500">
-                Última actualizacíon hace: Platanito
-              </h6>
-            </div>
-            <div>
-              {schedule.length > 0 ? (
-                <ul className="space-y-4">
-                  {schedule.map((item) => (
-                    <li
-                      key={item.id}
-                      className="flex items-center p-4 bg-gray-100 dark:bg-zinc-900 dark:border border-zinc-800 rounded-lg overflow-x-auto"
-                    >
-                      <div className="flex-shrink-0 mr-4">
-                        <Clock startTime={item.startTime} />
-                      </div>
-                      <div className="flex-grow">
-                        <h3 className="font-semibold">{item.name}</h3>
-                        <p className="text-sm text-gray-500">
-                          {item.startTime} - {item.endTime}
-                        </p>
-                      </div>
-                      <div>
-                        <p className="">
-                          {(() => {
-                            let style;
-                            switch (item.building) {
-                              case "A":
-                                style =
-                                  "bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400";
-                                  console.log(item);
-                                break;
-                              case "B":
-                                style =
-                                  "bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-400 border border-yellow-500";
-                                break;
+	return (
+		<div className="h-screen">
+			<Nav />
+			<div className="container mx-auto">
+				<main className="flex-grow container mx-auto px-4 py-8">
+					<div className="border rounded-lg shadow-md p-6 bg-white dark:bg-black">
+						<h1 className="text-3xl font-semibold text-center my-2">
+							Horario Peñalolen
+						</h1>
+						<div className="border-b pb-4 mb-4">
+							<h3 className="text-black text-lg font-bold text-center mx-0 w-full dark:text-gray-200">
+								{formatDate(currentDate)}
+							</h3>
+							<h6 className="text-center text-sm mx-0 w-full text-zinc-500">
+								Última actualizacíon hace: Platanito
+							</h6>
+						</div>
+						<div>
+							{schedule.length > 0 ? (
+								<ul className="space-y-4">
+									{schedule.map((item) => (
+										<li
+											key={item.id}
+											className="flex items-center p-4 bg-gray-100 dark:bg-zinc-900 dark:border border-zinc-800 rounded-lg overflow-x-auto"
+										>
+											<div className="flex-shrink-0 mr-4">
+												<Clock startTime={item.startTime} />
+											</div>
+											<div className="flex-grow">
+												<h3 className="font-semibold">{item.name}</h3>
+												<p className="text-sm text-gray-500">
+													{item.startTime} - {item.endTime}
+												</p>
+											</div>
+											<div>
+												<p className="">
+													{(() => {
+														let style;
+														switch (item.building) {
+															case "A":
+																style =
+																	"bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400";
+																console.log(item);
+																break;
+															case "B":
+																style =
+																	"bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-400 border border-yellow-500";
+																break;
 
-                              case "C":
-                                style =
-                                  "bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-500";
-                                break;
+															case "C":
+																style =
+																	"bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-500";
+																break;
 
-                              default:
-                                style =
-                                  "bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500";
-                                break;
-                            }
-                            return (
-                              <span className={style}>{item.location}</span>
-                            );
-                          })()}
-                        </p>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="text-center text-gray-500 dark:text-gray-200">
-                  No classes scheduled for today.
-                </p>
-              )}
-            </div>
-          </div>
-        </main>
-      </div>
-    </div>
-  );
+															default:
+																style =
+																	"bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500";
+																break;
+														}
+														return (
+															<span className={style}>{item.location}</span>
+														);
+													})()}
+												</p>
+											</div>
+										</li>
+									))}
+								</ul>
+							) : (
+								<p className="text-center text-gray-500 dark:text-gray-200">
+									No classes scheduled for today.
+								</p>
+							)}
+						</div>
+					</div>
+				</main>
+			</div>
+		</div>
+	);
 }
 
 const Clock = ({ startTime }: { startTime: string }) => {
-  const [hours, minutes] = startTime.split(":").map(Number);
-  const hourDeg = (hours % 12) * 30 + minutes * 0.5; // 360 degrees / 12 hours
-  const minuteDeg = minutes * 6; // 360 degrees / 60 minutes
+	const [hours, minutes] = startTime.split(":").map(Number);
+	const hourDeg = (hours % 12) * 30 + minutes * 0.5; // 360 degrees / 12 hours
+	const minuteDeg = minutes * 6; // 360 degrees / 60 minutes
 
-  return (
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 100 100"
-      className="relative stroke-black dark:stroke-white"
-    >
-      <circle
-        cx="50"
-        cy="50"
-        r="45"
-        strokeWidth="5"
-        className="fill-white stroke-black dark:stroke-white dark:fill-black "
-      />
-      <circle cx="50" cy="50" r="2" className="fill-black dark:fill-white" />
-      {/* Minute hand */}
-      <line
-        x1="50"
-        y1="50"
-        x2="50"
-        y2="15"
-        stroke=""
-        strokeWidth="5" // Thicker line for minute hand
-        transform={`rotate(${minuteDeg}, 50, 50)`}
-      />
-      {/* Hour hand */}
-      <line
-        x1="50"
-        y1="50"
-        x2="50"
-        y2="25"
-        strokeWidth="5"
-        transform={`rotate(${hourDeg}, 50, 50)`}
-      />
-    </svg>
-  );
+	return (
+		<svg
+			width="48"
+			height="48"
+			viewBox="0 0 100 100"
+			className="relative stroke-black dark:stroke-white"
+		>
+			<circle
+				cx="50"
+				cy="50"
+				r="45"
+				strokeWidth="5"
+				className="fill-white stroke-black dark:stroke-white dark:fill-black "
+			/>
+			<circle cx="50" cy="50" r="2" className="fill-black dark:fill-white" />
+			{/* Minute hand */}
+			<line
+				x1="50"
+				y1="50"
+				x2="50"
+				y2="15"
+				stroke=""
+				strokeWidth="5" // Thicker line for minute hand
+				transform={`rotate(${minuteDeg}, 50, 50)`}
+			/>
+			{/* Hour hand */}
+			<line
+				x1="50"
+				y1="50"
+				x2="50"
+				y2="25"
+				strokeWidth="5"
+				transform={`rotate(${hourDeg}, 50, 50)`}
+			/>
+		</svg>
+	);
 };
